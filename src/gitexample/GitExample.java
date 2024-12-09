4f22a987cdfc82926670883f2ecbc5a00939054a
pablonieres <elcharrua1975@gmail.com>
2024-12-09 18:49:28 -0300

    Segundo Commit

--- src/gitexample/GitExample.java
+++ src/gitexample/GitExample.java
@@ -4,17 +4,12 @@
  */
 package gitexample;
 
-/**
- *
- * @author Pablo Sena Nieres
- */
+
 public class GitExample {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello Uruguay");
     }
     
 }
